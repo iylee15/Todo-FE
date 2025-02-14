332e247183520db9b7d111eb1f35ba2575642eed
iylee15 <iylee153@gmail.com>
2025-02-14 22:46:13 +0900

    2025.02.14 헤더 내용 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='Header'>
-            <h3>오늘의 Plan🎃</h3>
+            <h3>나의 Plan🎃</h3>
             <h1>{new Date().toDateString()}</h1>
         </div>
     );
